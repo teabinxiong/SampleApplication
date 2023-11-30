bf3f6ebac00fdfcebb0afa7dc48dfc5f9d746b17
DOMOCI  | Tea Bin Xiong <65224341+teabinxiong@users.noreply.github.com>
2023-12-01 02:21:20 +0800

    first commit

---
+++ SampleApplication/Controllers/AuthenticationController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace SampleApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        // write your controller here
+
+
+
+        private string GenerateJSONWebToken( )
+        {
+           // write your generate token method here
+        }
+
+      
+    }
+}
